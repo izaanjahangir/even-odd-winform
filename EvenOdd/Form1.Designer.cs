56aa331d26237a81822bc1936ebd82a5e96ba4e9
izaan jahangir <izaanjahangir2@gmail.com>
2022-03-31 19:16:51 +0500

    project done

---
+++ EvenOdd/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace EvenOdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.output_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Check ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output_label
+            // 
+            this.output_label.AutoSize = true;
+            this.output_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_label.Location = new System.Drawing.Point(253, 192);
+            this.output_label.Name = "output_label";
+            this.output_label.Size = new System.Drawing.Size(51, 20);
+            this.output_label.TabIndex = 6;
+            this.output_label.Text = "label2";
+            this.output_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.output_label.Visible = false;
+            this.output_label.Click += new System.EventHandler(this.output_label_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(595, 331);
+            this.Controls.Add(this.output_label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Even Odd";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label output_label;
+    }
+}
+
---
+++ EvenOdd/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EvenOdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String inputtedNum = this.textBox1.Text;
+            int num;
+
+            if(inputtedNum.Length == 0)
+            {
+                showError("Please enter a number");
+                return;
+            }
+
+            if (!int.TryParse(inputtedNum, out num)) {
+                showError("Please enter a valid number");
+                return;
+            }
+
+            if(num % 2 == 0)
+            {
+                showSuccess(num.ToString() + " is a even number");
+               
+            }
+            else
+            {
+                showSuccess(num.ToString() + " is an odd number");
+
+            }
+        }
+
+        private void showError(String message)
+        {
+            this.output_label.Text = message;
+            this.output_label.Visible = true;
+            this.output_label.ForeColor = Color.Red;
+        }
+
+        private void showSuccess(String message)
+        {
+            this.output_label.Text = message;
+            this.output_label.Visible = true;
+            this.output_label.ForeColor = Color.Green;
+        }
+
+        private void output_label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
